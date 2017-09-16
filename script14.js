ca7bf94542eacd157b5f6bdd6dc65b47c16b7bd4
Mechanicalbird <mechanicalbird_just@yahoo.com>
2017-09-15 21:44:56 -0700

    Create script14.js

---
+++ script14.js
@@ -0,0 +1,9 @@
+$(function() {
+   // start up code goes here 
+   //alert("this works!");
+
+   //$("#title").text("Yey, I can now get at the H1 immediately");
+
+   $("#first").prepend("<h2>Great quotes</h2>");
+
+});
